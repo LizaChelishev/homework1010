ff477fad837ce1c4bd7411922fcd30f58d2764a4
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-09 12:26:55 +0300

    Create page25_11.py

---
+++ page25_11.py
@@ -0,0 +1,9 @@
+# input an integer number and print the number of digits
+tens = 10
+number_of_digits = 1
+number = int(input('input an integer number:'))
+while number // tens >= 1:
+    number_of_digits += 1
+    tens *= 10
+    if number // tens < 1:
+        print('The number of digits is: ' + str(number_of_digits))
